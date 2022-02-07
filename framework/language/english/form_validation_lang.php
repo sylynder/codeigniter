954fee645de56e1c84c6b366a6322b228eb6e4c7
otengkwame <developerkwame@gmail.com>
2022-02-07 15:45:56 +0000

    :ok_hand: add new validation rules and improve form validation
    Signed-off-by: otengkwame <developerkwame@gmail.com>

--- framework/language/english/form_validation_lang.php
+++ framework/language/english/form_validation_lang.php
@@ -67,3 +67,30 @@ $lang['form_validation_greater_than']		= 'The {field} field must contain a numbe
 $lang['form_validation_greater_than_equal_to']	= 'The {field} field must contain a number greater than or equal to {param}.';
 $lang['form_validation_error_message_not_set']	= 'Unable to access an error message corresponding to your field name {field}.';
 $lang['form_validation_in_list']		= 'The {field} field must be one of: {param}.';
+
+// --------------------------------------------------------------------
+
+$lang['file_required']                    = 'The field {field} is required.';
+$lang['file_size_max']                    = "The file uploaded in {field} is too big (max size is {param}).";
+$lang['file_size_min']                    = "The file uploaded in {field} is too small (min size is {param}).";
+$lang['file_allowed_type']                = "The file uploaded in {field} should be {param}.";
+$lang['file_disallowed_type']             = "The file uploaded in {field} cannot be {param}.";
+$lang['file_image_maxdim']                = "The dimensions of the file uploaded in {field} are too big.";
+$lang['file_image_mindim']                = "The dimensions of the file uploaded in {field} are too small.";
+$lang['file_image_exactdim']              = "The file uploaded in {field} doesn't have the right dimensions.";
+$lang['is_exactly']                       = "The field {field} contains a non valid value.";
+$lang['is_not']                           = "The field {field} contains a non valid value.";
+$lang['error_max_filesize_phpini']        = "The file exceeds the upload_max_filesize directive from php.ini.";
+$lang['error_max_filesize_form']          = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.";
+$lang['error_partial_upload']             = "The file it's only partially uploaded.";
+$lang['error_temp_dir']                   = "Temp dir error.";
+$lang['error_disk_write']                 = "Disk write error.";
+$lang['error_stopped']                    = "File upload stopped by extension";
+$lang['error_unexpected']                 = "Unexpected file upload error. Error: {field}";
+$lang['valid_json']                       = "The field {field} must be a valid json.";
+$lang['valid_hour']                       = "The field {field} must be a valid hour.";
+$lang['valid_date']                       = "The field {field} must be a valid date.";
+$lang['valid_range_date']                 = "The field {field} must be a valid date range.";
+$lang['valid_latitude']                   = "The %s latitude doesn't have a correct position.";
+$lang['valid_longitude']                  = "The %s longitude doesn't have a correct position.";
+$lang['valid_latlong']                    = "The %s map coodinates doesn't have a correct position.";
